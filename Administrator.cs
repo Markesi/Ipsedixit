9e67e0b7400a30b71242f16f363e12a966571376
Michelangelo Marchesi <michelangelo.marchesi@gmail.com>
2019-11-30 13:45:12 +0200

    Created User and Administrator classes

---
+++ Administrator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+/* Class Administrator
+ * 
+ * Codebase of Ixedixi
+ * Owner Michelangelo Marchesi
+ * Creation date: 2019-11-15
+ * Last change: 2019-11-16 
+ *
+ */
+
+// The Administrator class descends from the Teacher class (User class model)
+// Administrator enrolls students in the class (per school year)
+// Administrator creates users and update their data
+// Administrator imports new wordlists and correct the errors in it
+// manages it's data and has the privileges of the role
+// 
+// But Administrator cannot solve the exercises
+// nor assign exercise, review solutions,
+// Administator uses the message system (write, read)
+// 
+
+namespace Ixedixi
+{
+    class Administrator
+    {
+    }
+}
---
+++ User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Class User - model not implementable 
+ * Codebase of Ixedixi 
+ * Owner Michelangelo Marchesi
+ * Creation date: 2019-11-15
+ * Last change: 2019-11-16 
+ * 
+ */
+
+namespace Ixedixi
+{
+
+    // The User class provides common constructs to Students and Teachers (Administrators)
+    // managin data and access to the software according to their roles
+
+    class User
+    {
+    }
+}
